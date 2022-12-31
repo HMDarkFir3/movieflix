f05a6ff083cf7770325d7e5c90ff0ef2ba2ef19a
Henrique Marques <henrique.oliveira@mktsystems.com>
2022-12-31 12:21:55 -0300

    config: add app theme

---
+++ src/themes/dark.ts
@@ -0,0 +1,10 @@
+export const dark = {
+  colors: {
+    background: "#0e0e15",
+    primary: "#f31233",
+    secondary: "#e1e2eb",
+    tertiary: "#1b1a23",
+    text: "#73727a",
+    star: "#feaa2b",
+  },
+};
